d53bf2c206f0b163803acd9a5504515e832e806a
Piyush Kulkarni <pjkkulkarnipiyush@gmail.com>
2024-05-06 11:57:26 +0530

    createOne removed

--- routes/Product.js
+++ routes/Product.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, createone } = require("../controllers/Product");
+const { create } = require("../controllers/Product");
 const { auth } = require("../middlewares/Auth");
 
 
 router.post("/create" , auth , create);
-router.post("/createone" , auth , createone);
 
 module.exports = router;
